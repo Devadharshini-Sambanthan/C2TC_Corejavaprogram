0eea2a05e5e09fd3eb7a7ea7baf9b084d817a676
ASUS <ASUS@DEVA.mshome.net>
2025-08-08 09:57:18 +0530

    This contains core java programs

---
+++ C2TC_CoreJavaProgram/src/daytwenty/v1/Employee.java
@@ -0,0 +1,57 @@
+package daytwenty.v1;
+
+public class Employee {
+	private int empId;
+	private String name;
+	private String designation;
+	private float salary;
+	
+	public Employee() {
+
+	}
+	public Employee(int empId,String name,String Designation,float salary) {
+		super();
+		this.empID=empId;
+		this.name=name;
+		this.designation=designation;
+		this.salary=salary;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId=empId;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", designation=" + designation + ", salary=" + salary
+				+ "]\n";
+	}
+
+	}
+
